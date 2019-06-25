fcbef2defa16600fdd7ca7c32e84afae546cf790
Luca Stocchi <lstocchi@redhat.com>
2019-06-25 10:29:48 +0200

    removed the now ignored out directory

--- out/api/rspProviderAPI.d.ts
+++
@@ -1,5 +0,0 @@
-import { RSPServer } from "../util/types";
-export interface RSPProviderAPI {
-    registerRSPProvider(rsp: RSPServer): Promise<void>;
-    deregisterRSPProvider(id: string): Promise<void>;
-}
--- out/server/serverAPI.d.ts
+++
@@ -1,7 +0,0 @@
-import { ServerInfo } from "../util/types";
-export interface ServerAPI {
-    startRSP(stdoutCallback: (data: string) => void, stderrCallback: (data: string) => void): Promise<ServerInfo>;
-    stopRSP(): Promise<void>;
-    getHost(): string;
-    getPort(): number;
-}
