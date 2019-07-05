a4e60ff3fae8f00aa40b2fd7c27738ba818409ca
Luca Stocchi <lstocchi@redhat.com>
2019-07-05 11:11:23 +0200

    added getImage method to RSPController (#4)

--- src/server/rspController.ts
+++ src/server/rspController.ts
@@ -1,8 +1,10 @@
 import { ServerInfo } from '../util/types';
+import { Uri } from 'vscode';
 
 export interface RSPController {
     startRSP(stdoutCallback: (data: string) => void, stderrCallback: (data: string) => void ): Promise<ServerInfo>;
     stopRSP(): Promise<void>;
+    getImage(serverType: string): Uri;
     getHost(): string;
     getPort(): number;
 }
