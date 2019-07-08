3a1053a614dc9fe440f2b479a0f39624d462f6aa
Luca Stocchi <lstocchi@redhat.com>
2019-07-08 16:52:28 +0200

    added listener for rsp state

--- src/server/rspController.ts
+++ src/server/rspController.ts
@@ -5,6 +5,7 @@ export interface RSPController {
     startRSP(stdoutCallback: (data: string) => void, stderrCallback: (data: string) => void ): Promise<ServerInfo>;
     stopRSP(): Promise<void>;
     getImage(serverType: string): Uri;
+    onRSPServerStateChanged(listener: (state: number) => void): void;
     getHost(): string;
     getPort(): number;
 }
